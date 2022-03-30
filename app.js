a3713d606bf05587224499f32d5c7283ecc88f76
Jadson-Jose <jadsonjosesilva@gmail.com>
2022-03-30 11:12:41 -0300

    cont.

--- app.js
+++ app.js
@@ -12,8 +12,28 @@ function PageTransitions() {
             currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
             this.className += ' active-btn'
         })
-
     }
+
+    // Sections active class
+    addEventListener('click', (e) => {
+        const id = e.target.dataset.id;
+        if (id) {
+            // Remove selected from the other btns
+            sctBtns.forEach((btn) => {
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            // Hide other sections
+            sections.forEach((section) => {
+                section.classList.remove('active')
+            })
+
+            const element = document.getElementById(id);
+
+            element.classList.add('active')
+        }
+    })
 }
 
 PageTransitions();
